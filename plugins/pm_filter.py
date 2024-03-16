fb1878f1457f08ddd2cb203bb1bc99f7edeb9dd7
Babukuruvila <bobbyjohnamion@gmail.com>
2024-03-16 16:57:12 +0530

    back

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -9,7 +9,6 @@ lock = asyncio.Lock()
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
-from database.ia_filterdb import total_results
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, SUPPORT_CHAT_ID, CUSTOM_FILE_CAPTION, MSG_ALRT, PICS, AUTH_GROUPS, P_TTI_SHOW_OFF, GRP_LNK, CHNL_LNK, NOR_IMG, LOG_CHANNEL, SPELL_IMG, MAX_B_TN, IMDB, \
